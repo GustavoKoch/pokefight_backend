fc69f8c58405ac5bda42035958a084a346367a2e
Gustavo Koch <guzko@hotmail.com>
2022-07-09 16:55:15 +0200

    commit ready for deployment in heroku

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3001;
+const cors=require ('cors');
+
+app.use(cors());
+
+//For the data
+const pokemonslist=require("./data/pokemonslist");
 
 app.get('/', (req, res) => {
+    console.log("welcome");
   res.send('Welcome to the Pokefight Database')
 })
 
+app.get('/pokemon', (req, res) => {
+    console.log("Hola");
+    res.json(pokemonslist);
+  })
+
+app.get("/pokemon/:id", (req, res) => {
+    const { id } = req.params;
+    console.log(req.params);
+    const onePokemon = pokemonslist.find((x) => x.id === Number(req.params.id));
+
+    if (onePokemon) {
+        res.json(onePokemon);
+        //By Default express sends 200 all the time
+      } else {
+        res.status(404).send("The requested Pokemon doesn't exists");//404 Ressource doesn't exist or was found
+      }
+      }); 
+
+
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Pokemons app listening on port ${port}`)
 })
